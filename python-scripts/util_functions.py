d2712cb048c1f32cde7719739a515e233af16c63
DavidIlnytskyi <ilnytskyy.d@gmail.com>
2025-04-07 16:03:22 +0300

    Update log function

--- python-scripts/util_functions.py
+++ python-scripts/util_functions.py
@@ -1,7 +1,12 @@
 import os
 import sys
+from datetime import datetime
+
 
 def write_log(message: str, port: int):
+    current_time = str(datetime.now().time())
+    current_time = current_time[:current_time.find(".")]
+
     log_dir = "./logs"
     os.makedirs(log_dir, exist_ok=True)
 
@@ -9,5 +14,6 @@ def write_log(message: str, port: int):
     log_path = os.path.join(log_dir, f"{script_name}-{port}.txt")
 
     with open(log_path, "a", encoding="utf-8") as log_file:
-        log_file.write(message + "\n")
+        log_file.write(current_time + ": " + message + "\n")
+
 
